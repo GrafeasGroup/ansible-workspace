c1b0d017b2738db25f99b020b8bc5aa5a1c20140
David Alexander <opensource@thelonelyghost.com>
2019-11-24 02:07:32 -0500

    Adds automated tests for os_base role

--- roles/os_base/molecule/default/tests/test_default.py
+++ roles/os_base/molecule/default/tests/test_default.py
@@ -7,9 +7,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_journald_persists_logging(host):
+    f = host.file('/var/log/journald')
+    journald = host.service('systemd-journald')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.is_dir
+    assert journald.is_running
+    assert journald.is_enabled
+
+
+def test_admin_users(host):
+    sudoers = host.file('/etc/sudoers.d/sudoers')
+    assert sudoers.contains('%sudo ALL=(ALL)')
+    assert sudoers.mode == 0o644
